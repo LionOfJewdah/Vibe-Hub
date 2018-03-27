4f3ce5742a72f912fbe4acf9267ada1f63a96fd6
David Silverstone <dsilvers@usc.edu>
2018-03-26 23:42:04 -0700

    slightly more logging

--- barDataReceiver.js
+++ barDataReceiver.js
@@ -28,6 +28,7 @@ db.once('open', function() {
 
 function setupMQTTListener(mqtt_port, host, topic_path, callback) {
 	var client = mqtt.connect({host: host, port: mqtt_port});
+	console.log("MQTT client connected on port", mqtt_port);
 	client.subscribe(topic_path + "+");
 	client.on('message', callback);
 	return client;
