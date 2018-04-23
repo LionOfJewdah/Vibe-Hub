c3df7b10c9d0b6b32011cf2719e477ec48e48b5b
David Silverstone <dsilvers@usc.edu>
2018-04-22 20:16:58 -0700

    fix for thing I messed up

--- database/models/Sensor.js
+++ database/models/Sensor.js
@@ -6,6 +6,6 @@ var SensorSchema = new mongoose.Schema({
   kind_of_sensor: String, // TODO: verify e.g. camera, microphone, ultrasonic
   kind_ID: Number, // foreign key, i.e. the Camera ID or Mic ID primary key in another schema
   venue_ID: Number
-}, {timestamps: true, versionKey: false});
+});
 
 module.exports = mongoose.model('Sensor', SensorSchema);
